60d00dde8a72fe7e9c61d03d1647ca8ddc7c21b8
Vaibhav Lakhera <vaibhavlakhera1729@gmail.com>
2026-03-10 19:18:55 +0530

    hardcoded values in the plotting script

--- 19_plot_latency.py
+++ 19_plot_latency.py
@@ -1,11 +1,12 @@
-import pandas as pd
 import matplotlib.pyplot as plt
 
-data = pd.read_csv("19_results.csv")
+different_node = [0.001270, 0.001363, 0.001296, 0.001424, 0.001318, 0.001291, 0.001330, 0.002543]
+same_node = [0.001514, 0.001385, 0.001332, 0.001479, 0.001346, 0.001252, 0.001222, 0.001278]
 
-avg = data.groupby("placement")["latency"].mean()
+placements = ["different-node", "same-node"]
+avg_latency = [sum(different_node) / len(different_node), sum(same_node) / len(same_node)]
 
-avg.plot(kind="bar")
+plt.bar(placements, avg_latency)
 
 plt.ylabel("Average Latency (seconds)")
 plt.title("Pod Placement vs Latency")
